9aaa15c7bc5f4e4360e7d3d32ed9ca124d7bec1d
YoungJunRoh <shdudwns59@gmail.com>
2024-05-30 10:29:55 +0900

    변경했어요

--- src/Pages/MyPage.js
+++ src/Pages/MyPage.js
@@ -6,25 +6,31 @@ import dummyTweets from '../static/dummyData';
 
 const MyPage = () => {
   const filteredTweets = dummyTweets;
+  const currentUserTweets = filteredTweets.filter((tweet) => {
+    return tweet.username === 'parkhacker';
+  })
+  const currentUsername = currentUserTweets.length === 0 ? "Unknown" : currentUserTweets[0].username;
+  const currentUserPicture =currentUserTweets.length === 0 ? "https://www.google.com/imgres?q=%EA%B8%B0%EB%B3%B8%20%ED%94%84%EB%A1%9C%ED%95%84%20&imgurl=https%3A%2F%2Fmblogthumb-phinf.pstatic.net%2FMjAyMDExMDFfMTgy%2FMDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg%2F%25EC%259C%25A0%25ED%258A%259C%25EB%25B8%258C_%25EA%25B8%25B0%25EB%25B3%25B8%25ED%2594%2584%25EB%25A1%259C%25ED%2595%2584_%25ED%2595%2598%25EB%258A%2598%25EC%2583%2589.jpg%3Ftype%3Dw800&imgrefurl=https%3A%2F%2Fm.blog.naver.com%2Fgambasg%2F222132751279&docid=UwRQoM5gKXrlvM&tbnid=6uo_w__GpOOPDM&vet=12ahUKEwjZl5ThjLSGAxXpb_UHHfuENUUQM3oECCEQAA..i&w=800&h=800&hcb=2&ved=2ahUKEwjZl5ThjLSGAxXpb_UHHfuENUUQM3oECCEQAA" : currentUserTweets[0].picture;
   // TODO : 주어진 트윗 목록(dummyTweets)중 현재 유져인 parkhacker의 트윗만 보여줘야 합니다.
+
 
   return (
     <section className="myInfo">
       <div className="myInfo__container">
         <div className="myInfo__wrapper">
           <div className="myInfo__profile">
-            <img src={filteredTweets[1].picture} />
+            <img src={currentUserPicture} />
           </div>
           <div className="myInfo__detail">
             <p className="myInfo__detailName">
-              {filteredTweets[1].username} Profile
+              {currentUsername} Profile
             </p>
             <p>28 팔로워 100 팔로잉</p>
           </div>
         </div>
       </div>
       <ul className="tweets__mypage">
-        <Tweet tweet={filteredTweets[0]}/>
+        {currentUserTweets.map((tweet) => {return (<Tweet key={tweet.id} tweet={tweet}/>)})}
         {/* TODO : 주어진 트윗 목록(dummyTweets)중 현재 유져인 parkhacker의 트윗만 보여줘야 합니다. */}
       </ul>
       <Footer />
--- src/Pages/Tweets.js
+++ src/Pages/Tweets.js
@@ -1,25 +1,46 @@
 // TODO : useState를 react로 부터 import 합니다.
-import React, { /* TODO */ } from 'react';
+import React, { useState } from 'react';
 import Footer from '../Footer';
 import Tweet from '../Components/Tweet';
 import './Tweets.css';
 import dummyTweets from '../static/dummyData';
 
+
 const Tweets = () => {
   // TODO : 새로 트윗을 작성하고 전송할 수 있게 useState를 적절히 활용하세요.
+  const [tweets, setTweets] = useState(dummyTweets);
+  const [username, setUsername] = useState('');
+  const [msg, setMsg] = useState('');
+  
+  const getRandomNumber = (min, max) => {
+    return parseInt(Math.random() * (Number(max) - Number(min) + 2));
+  };
 
   const handleButtonClick = (event) => {
-    const tweet = {};
+    const tweet = {
+      id : tweets.length + 1,
+      username : username,
+      picture : `https://randomuser.me/api/portraits/women/${getRandomNumber(
+        1,
+        98
+      )}.jpg`,
+      content : msg,
+      createdAt : new Date().toLocaleDateString('ko-KR'),
+      updatedAt : new Date().toLocaleDateString('ko-KR')
+    };
     // TODO : Tweet button 엘리먼트 클릭시 작동하는 함수를 완성하세요.
     // 트윗 전송이 가능하게 작성해야 합니다.
+    setTweets([tweet, ...tweets]);
   };
 
   const handleChangeUser = (event) => {
     // TODO : Tweet input 엘리먼트에 입력 시 작동하는 함수를 완성하세요.
+    setUsername(event.target.value);
   };
 
   const handleChangeMsg = (event) => {
     // TODO : Tweet textarea 엘리먼트에 입력 시 작동하는 함수를 완성하세요.
+    setMsg(event.target.value);
   };
 
   return (
@@ -37,19 +58,27 @@ const Tweets = () => {
                   defaultValue="parkhacker"
                   placeholder="your username here.."
                   className="tweetForm__input--username"
+                  value ={username}
+                  onChange={handleChangeUser}
                 ></input>
-                TODO : 트윗을 작성할 수 있는 textarea 엘리먼트를 작성하세요.
+                <textarea 
+                className='tweetForm__input--message'
+                placeholder='your tweet here..'
+                value ={msg}
+                onChange={handleChangeMsg}
+                >
+                </textarea>
               </div>
               <div className="tweetForm__count" role="status">
                 <span className="tweetForm__count__text">
-                  {/* TODO : 트윗 총 개수를 보여줄 수 있는 Counter를 작성하세요. */}
-                  {'total: '}
+                  {'total: ' + tweets.length}
                 </span>
               </div>
             </div>
             <div className="tweetForm__submit">
               <div className="tweetForm__submitIcon"></div>
               {/* TODO : 작성한 트윗을 전송할 수 있는 button 엘리먼트를 작성하세요. */}
+              <button className='tweetForm__submitButton' onClick={handleButtonClick}>tweet</button>
             </div>
           </div>
         </div>
@@ -57,7 +86,9 @@ const Tweets = () => {
       <div className="tweet__selectUser"></div>
       <ul className="tweets">
         {/* TODO : 하나의 트윗이 아니라, 주어진 트윗 목록(dummyTweets) 갯수에 맞게 보여줘야 합니다. */}
-        <Tweet tweet={dummyTweets[0]} />
+        {tweets.map((tweet) =>{
+          return <Tweet key= {tweet.id} tweet = {tweet} />
+        })}
       </ul>
       <Footer />
     </React.Fragment>
